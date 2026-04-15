6562a41730ee95a9ffa7448ba04acf7e5e629445
frl114571-create <frl114571@gmail.com>
2026-04-15 20:27:20 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const http = require('http'); http.createServer((req, res) => res.end('Bot is running')).listen(process.env.PORT || 3000);
 const mineflayer = require('mineflayer');
 
 const botArgs = {
